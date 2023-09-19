aa4e34e37144ca2a1e7363cb8f5bea2ec2ce759e
dawwson <shines1427@gmail.com>
2023-09-20 00:11:38 +0900

    Test: 테스트 전에 insert, 테스트 후에 truncate(with .sql 파일)

--- src/test/java/dawwson/todoappbe/controller/TodoApiControllerTest.java
+++ src/test/java/dawwson/todoappbe/controller/TodoApiControllerTest.java
@@ -1,31 +1,36 @@
 package dawwson.todoappbe.controller;
 
+import dawwson.todoappbe.domain.Todo;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import jakarta.persistence.EntityManager;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.Matchers.*;
 
-// TODO: 데이터 초기화 안 되는 문제 해결
 @SpringBootTest
+@Sql(scripts = {"/sql/insert.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = {"/sql/truncate.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 class TodoApiControllerTest {
     @Autowired EntityManager em;
 
     @Test
-    void 할_일_추가하기_성공() {
+    void 할_일_추가하기_성공() throws JSONException {
         // TODO: User 생성
-        Map<String, String> bodyMap = new HashMap<>();
-        bodyMap.put("content", "테스트임");
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("content", "이것은 테스트");
 
         RestAssured
                 // given
                 .given().log().all()
-                .body(bodyMap)
+                .body(requestBody.toString())
                 .contentType(ContentType.JSON)
                 // when
                 .when()
---
+++ src/test/resources/sql/insert.sql
@@ -0,0 +1,14 @@
+insert
+into todo (todo_id, is_done, content, created_at, updated_at)
+values ('fec21270-8704-460a-aec4-0d324efeb990',
+        false,
+        'test1',
+        now(),
+        now());
+insert
+into todo (todo_id, is_done, content, created_at, updated_at)
+values ('c0a17ae0-a224-48a7-9fc2-212f779157ce',
+        false,
+        'test2',
+        now(),
+        now());
