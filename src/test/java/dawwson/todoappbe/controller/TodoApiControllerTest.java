a7429edca920458bdd42938424250fc1b462295e
dawwson <shines1427@gmail.com>
2023-09-20 15:36:43 +0900

    Test: 전역으로 baseURI 설정, 조회 API 테스트 작성

--- src/test/java/dawwson/todoappbe/controller/TodoApiControllerTest.java
+++ src/test/java/dawwson/todoappbe/controller/TodoApiControllerTest.java
@@ -1,17 +1,15 @@
 package dawwson.todoappbe.controller;
 
-import dawwson.todoappbe.domain.Todo;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import jakarta.persistence.EntityManager;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.Matchers.*;
 
@@ -19,10 +17,47 @@ import static org.hamcrest.Matchers.*;
 @Sql(scripts = {"/sql/insert.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = {"/sql/truncate.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 class TodoApiControllerTest {
-    @Autowired EntityManager em;
+
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "http://127.0.0.1:8080/api/v1";
+    }
 
     @Test
-    void 할_일_추가하기_성공() throws JSONException {
+    void 할_일_조회_성공하면_200_응답을_보낸다() {
+        // given
+
+        // when
+        RequestSpecification request = RestAssured.given().log().uri();
+        Response response = request.get("/todos");
+
+        // then
+        response.then()
+                .assertThat()
+                .statusCode(200);
+        response.then()
+                .assertThat()
+                .contentType(ContentType.JSON);
+        response.then()
+                .assertThat()
+                .body("code", is(200))
+                .body("message", isA(String.class))
+                .body("data.size()", is(2));
+
+        response.then()
+                .assertThat()
+                .body("data[0].id", isA(String.class))
+                .body("data[0].isDone", isA(Boolean.class))
+                .body("data[0].content", isA(String.class))
+                .body("data[0].createdAt", isA(String.class))
+                .body("data[1].id", isA(String.class))
+                .body("data[1].isDone", isA(Boolean.class))
+                .body("data[1].content", isA(String.class))
+                .body("data[1].createdAt", isA(String.class));
+    }
+
+    @Test
+    void 할_일_추가하기_성공하면_201_응답을_보낸다() throws JSONException {
         // TODO: User 생성
         JSONObject requestBody = new JSONObject();
         requestBody.put("content", "이것은 테스트");
